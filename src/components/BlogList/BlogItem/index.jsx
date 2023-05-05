62a1477f3b6473c7ed269724546a14498f3bcc38
JaklinBshv <jb@auxcode.com>
2023-05-05 14:41:53 +0300

    fixed cards

--- src/components/BlogList/BlogItem/index.jsx
+++ src/components/BlogList/BlogItem/index.jsx
@@ -16,26 +16,25 @@ const BlogItem = ({
   },
 }) => {
   return (
-    <div className="blogItem-wrap">
-      <img className="blogItem-cover" src={cover} alt="cover" />
-      <div className="description">
-        <Chip label={category} />
-        <h3>{title}</h3>
-        <p className="blogItem-desc">{description}</p>
-      </div>
-      <footer>
-        <div className="blogItem-author">
-          <img src={authorAvatar} alt="avatar" />
-          <div>
-            <h6>{authorName}</h6>
-            <p>{createdAt}</p>
-          </div>
+    <Link className="blogItem-link" to={`/blog/${id}`}>
+      <div className="blogItem-wrap">
+        <img className="blogItem-cover" src={cover} alt="cover" />
+        <div className="description">
+          <Chip label={category} />
+          <h3>{title}</h3>
+          <p className="blogItem-desc">{description}</p>
         </div>
-        <Link className="blogItem-link" to={`/blog/${id}`}>
-          ➝
-        </Link>
-      </footer>
-    </div>
+        <footer>
+          <div className="blogItem-author">
+            <img src={authorAvatar} alt="avatar" />
+            <div>
+              <h6>{authorName}</h6>
+              <p>{createdAt}</p>
+            </div>
+          </div>
+        </footer>
+      </div>{" "}
+    </Link>
   );
 };
 
