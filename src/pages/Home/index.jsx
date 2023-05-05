77fa04d1e21db44d1c87d2b46d3f265ed26a5791
JaklinBshv <jb@auxcode.com>
2023-05-05 17:31:48 +0300

    added articles in home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import Header from "../../components/Home/Header";
+import EmptyList from "../../components/common/EmptyList";
+import BlogList from "../../components/BlogList";
+import { blogList } from "../../config/data";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(blogList);
+
   return (
     <div>
       <Header />
+      {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />}
     </div>
   );
 };
