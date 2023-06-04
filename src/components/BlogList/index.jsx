6740884ee1a84d8db5559594fd0379cfa0662ea4
JaklinBshv <jb@auxcode.com>
2023-06-04 16:10:42 +0300

    sort archive

--- src/components/BlogList/index.jsx
+++ src/components/BlogList/index.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
-import BlogItem from './BlogItem';
-import './styles.css';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import BlogItem from "./BlogItem";
+import "./styles.css";
 
 const BlogList = ({ blogs }) => {
+  const { t } = useTranslation("archivePage");
+
+  const [sortOption, setSortOption] = useState("none");
+
+  const handleSortChange = (event) => {
+    setSortOption(event.target.value);
+  };
+
+  const sortBlogs = (blogs) => {
+    if (sortOption === "oldest") {
+      return [...blogs].sort(
+        (a, b) => new Date(a.createdAt.seconds) - new Date(b.createdAt.seconds)
+      );
+    } else if (sortOption === "newest") {
+      return [...blogs].sort(
+        (a, b) => new Date(b.createdAt.seconds) - new Date(a.createdAt.seconds)
+      );
+    } else {
+      return blogs;
+    }
+  };
+
   return (
-    <div className='blogList-wrap'>
-      {blogs.map((blog) => (
-        <BlogItem blog={blog} />
-      ))}
+    <div className="blogList">
+      <div>
+        <select id="sortOption" value={sortOption} onChange={handleSortChange}>
+          <option value="none">{t("sort_by")}</option>
+          <option value="oldest">{t("oldest_first")}</option>
+          <option value="newest">{t("newest_first")}</option>
+        </select>
+      </div>
+      <div className="blogList-wrap">
+        {sortBlogs(blogs).map((blog) => (
+          <BlogItem blog={blog} key={blog.id} />
+        ))}
+      </div>
     </div>
   );
 };
