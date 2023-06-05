52bd64a19b99aa3e5084905f69dd2126e01c9666
JaklinBshv <jb@auxcode.com>
2023-06-05 21:15:11 +0300

    journal page design changes

--- src/pages/Journal/journal.jsx
+++ src/pages/Journal/journal.jsx
@@ -11,7 +11,8 @@ const Journal = () => {
   };
 
   const handleAddressClick = () => {
-    const googleMapsUrl = "https://www.google.com/maps/search/%D0%A2%D0%B5%D1%85%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8+%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82-%D0%A1%D0%BE%D1%84%D0%B8%D1%8F+%D0%A1%D0%BE%D1%84%D0%B8%D1%8F+1000,+%D0%B1%D1%83%D0%BB.%22%D0%9A%D0%BB.+%D0%9E%D1%85%D1%80%D0%B8%D0%B4%D1%81%D0%BA%D0%B8%22+8/@42.6570438,23.35319,17z/data=!3m1!4b1?entry=ttu";
+    const googleMapsUrl =
+      "https://www.google.com/maps/search/%D0%A2%D0%B5%D1%85%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8+%D1%83%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82-%D0%A1%D0%BE%D1%84%D0%B8%D1%8F+%D0%A1%D0%BE%D1%84%D0%B8%D1%8F+1000,+%D0%B1%D1%83%D0%BB.%22%D0%9A%D0%BB.+%D0%9E%D1%85%D1%80%D0%B8%D0%B4%D1%81%D0%BA%D0%B8%22+8/@42.6570438,23.35319,17z/data=!3m1!4b1?entry=ttu";
     window.open(googleMapsUrl, "_blank");
   };
 
@@ -49,12 +50,48 @@ const Journal = () => {
         </div>
       </div>
       <br />
-      <div className="paragraphs">
-        <p className="description">{t("detailed_description_p1")}</p>
-        <p className="description">{t("detailed_description_p2")}</p>
-        <p className="description">{t("detailed_description_p3")}</p>
-        <p className="description">{t("detailed_description_p4")}</p>
+
+      <div id="ct">
+        <div class="corner " id="left_top"></div>
+        <div class="corner" id="left_bottom"></div>
+        <div class="corner" id="right_top"></div>
+        <div class="corner" id="right_bottom"></div>
+        <blockquote>
+          <p className="more-description">{t("detailed_description_p1")}</p>
+        </blockquote>
       </div>
+
+      <div id="ct">
+        <div class="corner " id="left_top"></div>
+        <div class="corner" id="left_bottom"></div>
+        <div class="corner" id="right_top"></div>
+        <div class="corner" id="right_bottom"></div>
+        <blockquote>
+          <p className="more-description">{t("detailed_description_p2")}</p>
+        </blockquote>
+      </div>
+
+      <div id="ct">
+        <div class="corner " id="left_top"></div>
+        <div class="corner" id="left_bottom"></div>
+        <div class="corner" id="right_top"></div>
+        <div class="corner" id="right_bottom"></div>
+        <blockquote>
+          <p className="more-description">{t("detailed_description_p3")}</p>
+        </blockquote>
+      </div>
+
+
+      <div id="ct">
+        <div class="corner " id="left_top"></div>
+        <div class="corner" id="left_bottom"></div>
+        <div class="corner" id="right_top"></div>
+        <div class="corner" id="right_bottom"></div>
+        <blockquote>
+          <p className="more-description">{t("detailed_description_p4")}</p>
+        </blockquote>
+      </div>
+
     </div>
   );
 };
