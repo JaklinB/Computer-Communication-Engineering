c3e6652ab4d770b77a70cd561102e19744f6b946
JaklinBshv <jb@auxcode.com>
2023-06-11 16:45:36 +0300

    newest articles from blog

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Home/Header";
 import EmptyList from "../../components/common/EmptyList";
-import BlogList from "../../components/BlogList";
+import BlogItem from "../../components/BlogList/BlogItem";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "./styles.css";
 
-const Home = ({ isAdmin }) => {
- 
-  console.log(isAdmin);
+const Home = ({ isLoggedIn }) => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const firestore = firebase.firestore();
+        const snapshot = await firestore.collection("articles").get();
+        const articles = snapshot.docs.map((doc) => doc.data());
+        setBlogs(articles);
+      } catch (error) {
+        console.error("Error fetching articles: ", error);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  const newestBlogs = blogs.slice(0, 3);
+
   return (
     <div>
       <Header />
-      {/* {!blogs.length ? <EmptyList /> : <BlogList blogs={blogs} />} */}
+      <h1 className="blogList-title">Най-новите статии</h1>
+      <div className="blogList-wrap">
+          {!blogs.length ? (
+        <EmptyList />
+      ) : (
+        newestBlogs.map((blog) => (
+          <BlogItem
+            key={blog.id}
+            blog={blog}
+            isLoggedIn={isLoggedIn} 
+          />
+        ))
+      )}
+      </div>
+    
     </div>
   );
 };
