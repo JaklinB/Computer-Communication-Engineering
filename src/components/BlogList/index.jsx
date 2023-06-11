d4b7f18778af5b46036af4389ef16eba2e7cafde
JaklinBshv <jb@auxcode.com>
2023-06-11 12:00:26 +0300

    sort by likes

--- src/components/BlogList/index.jsx
+++ src/components/BlogList/index.jsx
@@ -15,11 +15,21 @@ const BlogList = ({ blogs, isLoggedIn }) => {
   const sortBlogs = (blogs) => {
     if (sortOption === "oldest") {
       return [...blogs].sort(
-        (a, b) => new Date(a.createdAt.seconds) - new Date(b.createdAt.seconds)
+        (a, b) =>
+          new Date(a.createdAt.seconds) - new Date(b.createdAt.seconds)
       );
     } else if (sortOption === "newest") {
       return [...blogs].sort(
-        (a, b) => new Date(b.createdAt.seconds) - new Date(a.createdAt.seconds)
+        (a, b) =>
+          new Date(b.createdAt.seconds) - new Date(a.createdAt.seconds)
+      );
+    } else if (sortOption === "most-liked") {
+      return [...blogs].sort(
+        (a, b) => (b.likeCount || 0) - (a.likeCount || 0)
+      );
+    } else if (sortOption === "least-liked") {
+      return [...blogs].sort(
+        (a, b) => (a.likeCount || 0) - (b.likeCount || 0)
       );
     } else {
       return blogs;
@@ -33,11 +43,13 @@ const BlogList = ({ blogs, isLoggedIn }) => {
           <option value="none">{t("sort_by")}</option>
           <option value="oldest">{t("oldest_first")}</option>
           <option value="newest">{t("newest_first")}</option>
+          <option value="most-liked">{t("most_liked")}</option>
+          <option value="least-liked">{t("least_liked")}</option>
         </select>
       </div>
       <div className="blogList-wrap">
         {sortBlogs(blogs).map((blog) => (
-          <BlogItem blog={blog} key={blog.id} isLoggedIn={isLoggedIn}/>
+          <BlogItem blog={blog} key={blog.id} isLoggedIn={isLoggedIn} />
         ))}
       </div>
     </div>
